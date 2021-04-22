b447128bed0dce64255ba54caccd77c4db9e646f
Manami Ueda <manamiueda1@gmail.com>
2021-04-22 18:16:45 -0400

    reviewed function types

--- function.ts
+++ function.ts
@@ -1,26 +1,37 @@
 // we can assign types to parameters
 // TypeScript will infer that the return type is a number
 // add : type after the paramters fo define the return type 
-function add(n1, n2) {
-    return n1 + n2;
+
+function add(n1: number, n2: number): number {
+  return n1 + n2
 }
+
 // void return type -> it does not return anything - it does not have a return statement
-function printResult(num) {
-    console.log('result: ' + num);
+
+function printResult(num: number): void {
+  console.log('result: ' + num);
+
 }
+
 // callback function example
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-printResult(add(5, 12));
+
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result)
+  }
+printResult(add(5, 12))
+
 // Function type example
 // combineValues accepts a function that takes in 2 parameters as a number and returns a number
-var combineValues;
+let combineValues: (a: number, b: number) => number;
+
 combineValues = add;
 // combineValues = printResult;
 // combineValues = 5;
-console.log(combineValues(8, 8));
-addAndHandle(10, 20, function (result) {
-    console.log(result);
+
+console.log(combineValues(8, 8))
+
+addAndHandle(10, 20, (result) => {
+  console.log(result)
+  return result;
 });
