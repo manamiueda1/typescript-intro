c49e5b1e39a9ecbb5937e45b005df8b0ddd0d710
Manami Ueda <manamiueda1@gmail.com>
2021-04-21 21:11:28 -0400

    union types

--- app.ts
+++ app.ts
@@ -1,58 +1,15 @@
-// enum example
-
-const ADMIN = 0;
-const READ_ONLY = 1;
-const AUTHOR = 2;
-
-// you can assign it's own numbers 
-// advantages of enum -> gives labels assigned numbers and you can reuse those labels 
-// access the number by using dot notation
-// use when you need identifiers that are human readable
-enum Role { ADMIN = 5, READ_ONLY, AUTHOR };
-
-const person = {
-  name: 'Max',
-  age: 30,
-  hobbies: ['sports', 'cooking'],
-  role: Role.ADMIN
+function combine(input1: number | string, input2: number | string) {
+  let result;
+  if (typeof input1 === 'number' && typeof input2 === 'number') {
+    result = input1 + input2
+  } else {
+    result = input1.toString() + input2.toString()
+  }
+    return result
 }
 
+const combineAges = combine(30, 26);
+console.log(combineAges)
 
-
-if (person.role === Role.AUTHOR) {
-  console.log('is admin')
-}
-
-
-
-// Tuple Example
-
-// const person: {
-//   name: string;
-//   age: number;
-//   hobbies: string[];
-//   role: [number, string];
-// } = { 
-//   name: 'Max',
-//   age: 30,
-//   hobbies: ['sports', 'cooking'],
-//   role: [2, 'author']
-// }
-
-// you can push into tuple data type
-// person.role.push('admin')
-// person.role[1] = 10;
-
-// you can update the length of tuple if you need to 
-// person.role = [0, 'admin', 'user'];
-
-// array of strings 
-let favoriteActivities: string[];
-//  if you want to have multiple types, use any[]
-favoriteActivities = ['Sports']
-
-console.log(person.name)
-
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase())
-}
+const combineNames = combine('Max', 'Anna');
+console.log(combineNames)
--- basics.ts
+++ basics.ts
@@ -5,7 +5,7 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string) {
   if (showResult) {
     console.log(phrase + result)
   } else {
-    return n1 + n2
+    return result
   }
 }
 
