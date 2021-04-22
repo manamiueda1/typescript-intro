10217c0419c4732968b4a77126e41ddabf13e237
Manami Ueda <manamiueda1@gmail.com>
2021-04-22 18:44:34 -0400

    reviewed unknown and never types

--- app.js
+++ app.js
@@ -1,26 +1,15 @@
-// we can assign types to parameters
-// TypeScript will infer that the return type is a number
-// add : type after the paramters fo define the return type 
-function add(n1, n2) {
-    return n1 + n2;
+// unknown type example
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Max';
+// you have to specify because if you just reassign, it will throw an error
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-// void return type -> it does not return anything - it does not have a return statement
-function printResult(num) {
-    console.log('result: ' + num);
+// never type example
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
 }
-// callback function example
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-printResult(add(5, 12));
-// Function type example
-// combineValues accepts a function that takes in 2 parameters as a number and returns a number
-var combineValues;
-combineValues = add;
-// combineValues = printResult;
-// combineValues = 5;
-console.log(combineValues(8, 8));
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-});
+var result = generateError('An error occured', 500);
+console.log(result);
---
+++ unknown-never.ts
@@ -0,0 +1,21 @@
+// unknown type example
+
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'Max'
+
+// you have to specify because if you just reassign, it will throw an error
+if (typeof userInput === 'string') {
+  userName = userInput
+}
+
+// never type example
+
+function generateError(message: string, code: number): never {
+  throw {message: message, errorCode: code};
+}
+
+const result = generateError('An error occured', 500)
+console.log(result);
